7570e4b1700036d5c055854c39c6e8bd9f2ad3b3
Gleb <git@gleb.nl>
2016-12-01 17:53:59 +0100

    Removed unused whitespace in chars array.

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 
 var app = {
 
-  chars: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  chars: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'],
 
   init: function () {
     var container = document.createElement('div');

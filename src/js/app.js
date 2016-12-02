513e13046e21555f51bd23860ba3b5e6a4af9ddb
Gleb <git@gleb.nl>
2016-12-02 16:46:01 +0100

    Added add and remove functionality for animated elements.

--- src/js/app.js
+++ src/js/app.js
@@ -5,29 +5,33 @@ var app = {
   chars: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'],
 
   init: function () {
-    var container = document.createElement('div');
-    document.body.appendChild(container);
-    container.className = 'animation-container';
+    app.container = document.createElement('div');
+    app.container.className = 'animation-container';
+    document.body.appendChild(app.container);
     for ( var i = 0; i < 50; i++ ) {
-      window.setTimeout(function () {
-        var element = document.createElement('span');
-        element.className = 'animation-element';
-        container.appendChild(element);
-        app.animate(element);
-      }, Math.floor(Math.random() * 15) * 1000);
+      window.setTimeout(app.add, Math.floor(Math.random() * 15) * 1000);
     }
+  },
+
+  add: function () {
+    var element = document.createElement('span');
+    app.container.appendChild(element);
+    app.animate(element);
+  },
+
+  remove: function (element) {
+    element.parentNode.removeChild(element);
+    app.add();
   },
 
   animate: function (element) {
     var character = app.chars[Math.floor(Math.random() * app.chars.length)];
-    var duration = Math.floor(Math.random() * 15);
+    var duration = Math.floor(Math.random() * 15) + 1;
     var offset = Math.floor(Math.random() * (30 - duration * 2)) + 3;
     var size = 10 + (15 - duration);
-    element.style.cssText = 'right:'+offset+'vw; font-size: '+size+'px; animation-duration:'+duration+'s';
+    element.style.cssText = 'right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
     element.innerHTML = character;
-    window.setTimeout(function (element) {
-      app.animate(element);
-    }, duration * 1000, element);
+    window.setTimeout(app.remove, duration * 1000, element);
   },
 
 };

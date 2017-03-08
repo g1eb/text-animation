17578b9bf4e3fc6a4dcb2b25c85d0e6d438e5892
Gleb <mail@g1eb.com>
2017-03-08 15:41:58 +0100

    Initiate 10 new elements each second, don't add on remove.

--- src/js/text-animation.js
+++ src/js/text-animation.js
@@ -8,20 +8,13 @@ var app = {
     app.container = document.createElement('div');
     app.container.className = 'animation-container';
     document.body.appendChild(app.container);
-    for ( var i = 0; i < 50; i++ ) {
-      window.setTimeout(app.add, Math.floor(Math.random() * 15) * 1000);
-    }
+    window.setInterval(app.add, 100);
   },
 
   add: function () {
     var element = document.createElement('span');
     app.container.appendChild(element);
     app.animate(element);
-  },
-
-  remove: function (element) {
-    element.parentNode.removeChild(element);
-    app.add();
   },
 
   animate: function (element) {
@@ -32,6 +25,10 @@ var app = {
     element.style.cssText = 'right:'+offset+'vw; font-size:'+size+'px;animation-duration:'+duration+'s';
     element.innerHTML = character;
     window.setTimeout(app.remove, duration * 1000, element);
+  },
+
+  remove: function (element) {
+    element.parentNode.removeChild(element);
   },
 
 };
